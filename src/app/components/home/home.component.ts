f17c91d77d788cf0d21e3bd88cea0dfd3a42498f
Hugo-Desain <hugodesain@yahoo.fr>
2025-01-17 16:18:24 +0100

    Formulaire authentification sur home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
-import { UserFormComponent } from '../user-form/user-form.component';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-home',
-  imports: [UserFormComponent, FormsModule],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styles: ``,
 })
 export class HomeComponent {
   name: string = '';
+  errorMessage: string = '';
+  user: User | undefined;
 
   constructor(private userService: UserService, private router: Router) {}
 
   onSubmit() {
     console.log("Nom d'utilisateur : " + this.name);
-    const user = this.userService.findUserByName(this.name);
-    if (user) {
-      this.router.navigate(['/todos']);
-    } else {
-      console.log("Nom d'utilisateur incorrect");
-    }
+    this.userService.findByName(this.name).subscribe((users) => {
+      if (users[0]) {
+        if (users[0].admin === false) {
+          this.router.navigate(['/users/list']);
+        } else {
+          this.router.navigate(['/todos']);
+        }
+      } else {
+        this.errorMessage = "L'utilisateur n'existe pas";
+      }
+    });
   }
 }
