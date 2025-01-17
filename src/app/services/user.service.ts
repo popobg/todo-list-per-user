0afa64d2943d6f65bfe6dd474efdc8662e706d4d
popobg <pauline.bouyssou1@gmail.com>
2025-01-17 14:27:22 +0100

    Fix routes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,18 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class UserService {
-  private readonly http = inject(HttpClient);
+    private readonly http = inject(HttpClient);
+    private readonly userUrl = 'http://localhost:3000/users';
 
-  findAll() {}
+    findAll() { }
 
-  findById() {}
+    findById() {
+        
+     }
 
-  createUser() {}
+    createUser() { }
 
-  updateUser() {}
+    updateUser() { }
 
-  deleteUser() {}
+    deleteUser() { }
 }
