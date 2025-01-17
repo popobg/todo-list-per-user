55801d759bf16a93b58bf309f2f0b07e161e817a
Mayran <loicmayran@gmail.com>
2025-01-17 12:03:47 +0100

    Ajout interface Todo et User

--- src/app/models/todo.ts
+++ src/app/models/todo.ts
@@ -1,2 +1,8 @@
 export interface Todo {
+    id: string;
+    txt: string;
+    done: boolean;
+    editable: boolean;
 }
+  
+export type Todos = Todo[];
--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,8 @@
 export interface User {
+    id: string;
+    nom: string;
+    description: string;
+    admin?: boolean;
 }
+    
+export type Users = User[];
